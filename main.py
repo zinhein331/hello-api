4e23141a9bddb88325e1a3c031200c7451bfd134
zinhein331 <69288072+zinhein331@users.noreply.github.com>
2021-10-01 22:01:48 +0630

    update CICD

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world_PythonGKE():
-    target = os.environ.get('TARGET', 'World')
+    target = os.environ.get('TARGET', 'World_CICD')
     return 'Hello {}!\n'.format(target)
 
 if __name__ == "__main__":
